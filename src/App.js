366da49cacd645ca6b3a0209b289eb435aa698ed
christopherhafke <chafke@umich.edu>
2018-10-07 20:45:06 -0400

    Adjusted readme, added framework for routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// TODO: Add the components these routes map to
+// TODO: Add a navbar
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>Navbar goes here</div>
+        <Router>
+          <div>
+            <Route exact path="/"/>
+            <Route path="/events"/>
+            <Route path="/faq"/>
+          </div>
+        </Router>
       </div>
     );
   }
