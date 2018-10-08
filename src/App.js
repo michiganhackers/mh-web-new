7b3683e60f34a15f5e0c76312441eed1f5975d2a
Ryan Siu <rsiu05@gmail.com>
2018-10-08 10:36:21 -0400

    Start implementing Calendar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Calendar from './components/Calendar.jsx';
 import './App.css';
+
+const divStyle = {
+  width: '50%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  display: 'block',
+  float: 'none'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={divStyle}>
+        <Calendar></Calendar>
       </div>
     );
   }
