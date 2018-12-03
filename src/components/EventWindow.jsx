d72fa477bce2d0e59cc10f869568823291b84502
Ryan Siu <rsiu05@gmail.com>
2018-12-03 14:24:25 -0500

    Handle close event window when click outside

--- src/components/EventWindow.jsx
+++ src/components/EventWindow.jsx
@@ -87,10 +87,35 @@ const CalendarTextContainer = styled.div`
 
 class EventWindow extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.props.closeWindow();
+    }
+  }
+
   render() {
     if (!this.props.hidden) {
       return (
-        <Wrapper className={this.props.className}>
+        <Wrapper className={this.props.className} ref={this.setWrapperRef}>
           <TitleContainer>
             <ToolbarContainer>
               <CalendarButton
