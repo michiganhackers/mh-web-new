21c0e9a45abacea0edf5aaf3aac6b85f71723cc1
Christopher Hafke <christopherhafke@0587410017.wireless.umich.net>
2020-01-08 15:15:26 -0500

    Changed links to OH signup

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ class About extends Component {
             <EventList>
               <li><a href="https://docs.google.com/presentation/d/122LR6C2yGpHYKe4mKHWFF98y6F8sV9Jh0dLnogiKnug/edit?usp=sharing">Our Mass Meeting Slides</a></li>
               <li><a href="https://docs.google.com/presentation/d/1ctHSblSXA9FiPauXlbR8ypy6dxF_zp4tH2ICU-_HKN0/edit">The Core Teams</a></li>
-              <li><a href="https://calendar.google.com/calendar/selfsched?sstoken=UUZvRjlEM0RMUlZ2fGRlZmF1bHR8NjkzODc3M2I3MDJlZGM5NDQyZDU2M2IzMzc4YTM2NjY">Office hours signup</a></li>
+              <li><a href="https://calendar.google.com/calendar/selfsched?sstoken=UVA3dk1tZkxNOFpCfGRlZmF1bHR8ODMwMzEwNjE4YzY3Y2I0OThlZGIzY2IwMGU1MDUwMGI">Office hours signup</a></li>
               <li><a href="https://docs.google.com/spreadsheets/d/13t39d5ou1FGmapBw2CxUsuo6OLtLENmbVQpotNGtCxM/edit#gid=0">Drop your LinkedIn</a></li>
               <li><a href="https://docs.google.com/document/d/1wchq66lN9bDLcSSAxPEamBQYqBjUVjfJgcd9NqBc704/edit">Some CS Resources</a></li>
               <li><a href="http://leccap.engin.umich.edu/leccap/site/7r31hdj317ufew776zq">Recordings</a></li>
--- src/components/Calendar/Events.jsx
+++ src/components/Calendar/Events.jsx
@@ -18,7 +18,7 @@ class Events extends Component {
         <SubpageOuter header="calendar">
           <Calendar calendarStyle={CalendarStyle}/>
           <a href="https://calendar.google.com/calendar?cid=OG44dTU4c3NyaWMxaG1tODRqdmt2bDlkNjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">Export/add our calendar!</a>
-          <a style={{float: "right"}} href="https://calendar.google.com/calendar/selfsched?sstoken=UUZvRjlEM0RMUlZ2fGRlZmF1bHR8NjkzODc3M2I3MDJlZGM5NDQyZDU2M2IzMzc4YTM2NjY">Office hours signup</a>
+          <a style={{float: "right"}} href="https://calendar.google.com/calendar/selfsched?sstoken=UVA3dk1tZkxNOFpCfGRlZmF1bHR8ODMwMzEwNjE4YzY3Y2I0OThlZGIzY2IwMGU1MDUwMGI">Office hours signup</a>
         </SubpageOuter>
       </React.Fragment>
     );
