2ec579c21d0aa2067384e16858e24bc4f1a072bc
Christopher Hafke <chafke@umich.edu>
2020-01-17 13:10:26 -0500

    Added better error message

--- src/components/MailingList/MailingList.jsx
+++ src/components/MailingList/MailingList.jsx
@@ -25,10 +25,11 @@ const EmailSubmitButton = styled.input`
 class MailingList extends React.Component {
     constructor(props){
         super(props);
-        this.state =  {address: '', submitted: false};
+        this.state =  {address: '', submitted: false, error: null};
 
         this.handleChange = this.handleChange.bind(this);
         this.addEmailToList = this.addEmailToList.bind(this);
+        this.renderSuccessOrFailure = this.renderSuccessOrFailure.bind(this);
     }
 
     addEmailToList(e){
@@ -46,20 +47,31 @@ class MailingList extends React.Component {
             url: mh_backend + '/v1/email/add',
             data: JSON.stringify(payload)
           }).then(res => {
-
+                this.setState({
+                    error: null,
+                    submitted: true,
+                    address: "",
+                })
             })
             .catch(error => {
-                console.log(error);
+                this.setState({
+                    error,
+                });
             });
-        
-        this.setState({
-            address: "",
-            submitted: true
-        })
     }
 
     handleChange(event) {
         this.setState({address: event.target.value});
+    }
+
+    renderSuccessOrFailure() {
+        if (this.state.error) {
+            return <StaticP> There was an error in adding your email. Contact michiganhackers-exec@umich.edu if this persists. </StaticP>
+        }
+        else if (this.state.submitted) {
+            return <StaticP> Successfully Added! </StaticP>;
+        }
+        return null;
     }
 
     render(){
@@ -68,7 +80,7 @@ class MailingList extends React.Component {
                 <StaticP>
                 Join our mailing list! You'll receive weekly updates from us detailing upcoming events, tech talks, hackathons, and news.
                 </StaticP>
-                {this.state.submitted ? <StaticP> Successfully Added! </StaticP> : null}
+                {this.renderSuccessOrFailure()}
                 <EmailInputBox type="email" value={this.state.address} onChange={this.handleChange} placeholder="michiganhackers@umich.edu"/>
                 <EmailSubmitButton type="submit" value="Join"/>
             </EmailForm>
