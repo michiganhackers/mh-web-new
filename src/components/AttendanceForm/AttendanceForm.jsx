567ad0477f89c6985d1858f39fa1d9936570df20
KindaOK <27974340+KindaOK@users.noreply.github.com>
2020-01-16 20:36:59 -0500

    Added location finding and checks if you are in the location area.
    The title says it all.

--- src/components/AttendanceForm/AttendanceForm.jsx
+++ src/components/AttendanceForm/AttendanceForm.jsx
@@ -22,13 +22,78 @@ const CodeSubmitButton = styled.input`
     margin-top: 10px;
 `
 
+// North Campus
+const NC_LEFT_LONG = -83.718946;
+const NC_RIGHT_LONG = -83.708453;
+const NC_TOP_LAT = 42.293946;
+const NC_BOTTOM_LAT = 42.287679;
+
+// TechArb
+const TA_LEFT_LONG = -83.743043;
+const TA_RIGHT_LONG = -83.742018;
+const TA_TOP_LAT = 42.280322;
+const TA_BOTTOM_LAT = 42.279035;
+
 class AttendanceForm extends React.Component {
     constructor(props){
         super(props);
-        this.state =  {code: '', submitted: false, uniqname: ''};
+        this.state =  {code: '', submitted: false, uniqname: '', location: {lat: null, lng: null}, is_in_location: false};
 
         this.handleChange = this.handleChange.bind(this);
         this.sendCode = this.sendCode.bind(this);
+        this.processLocationSignIn = this.processLocationSignIn.bind(this);
+    }
+
+    componentDidMount(props) {
+        this.setState({
+            userLocation: navigator.geolocation.getCurrentPosition(
+                position => {
+                    const {latitude, longitude} = position.coords;
+                    this.setState({
+                        location: {lat: latitude, lng: longitude}
+                    });
+
+                }
+            )
+        });
+    }
+
+    processLocationSignIn(e) {
+        e.preventDefault();
+
+        let mh_backend = process.env.REACT_APP_MH_BACKEND_URL;
+
+        if (this.state.location.lat > NC_BOTTOM_LAT && this.state.location.lat < NC_TOP_LAT && this.state.location.lng > NC_LEFT_LONG && this.state.location.lng < NC_RIGHT_LONG) {
+            // user is inside north campus fence
+            this.state.is_in_location = true;
+        }
+
+        if (this.state.location.lat > TA_BOTTOM_LAT && this.state.location.lat < TA_TOP_LAT && this.state.location.lng > TA_LEFT_LONG && this.state.location.lng < TA_RIGHT_LONG) {
+            // user is inside TechArb
+            this.state.is_in_location = true;
+        }
+        //handle location not working here
+        if (!this.state.is_in_location) return;
+
+        let payload = {
+            uniqname: this.state.uniqname
+        };
+
+        axios({
+            method: 'post',
+            headers: { 'content-type': 'application/json' },
+            url: mh_backend + '/v1/',
+            data: JSON.stringify(payload)
+        }).then(res => {
+
+        })
+            .catch(error => {
+                console.log(error);
+            });
+
+        this.setState({
+            submitted: true
+        })
     }
 
     sendCode(e){
@@ -65,13 +130,15 @@ class AttendanceForm extends React.Component {
 
     render(){
         return(
-            <EmailForm onSubmit={this.sendCode}>
+            <EmailForm onSubmit={this.state.location.lat ? this.processLocationSignIn : this.sendCode}>
                 <StaticP>
-                Input the attendance code after signing in using your google account.
+                Input the attendance code after signing in using your google account or sign in using your location.
                 </StaticP>
-                {this.state.submitted ? <StaticP> Successfully Added! </StaticP> : null}
-                <CodeInputBox type="email" value={this.state.code} onChange={this.handleChange} placeholder="Enter Code..."/>
-                <CodeSubmitButton type="submit" value="Submit"/>
+                {this.state.is_in_location ? <StaticP>You are in the location area</StaticP> : null}
+                {this.state.location.lat ? <CodeSubmitButton type="submit" value="Submit your location"/> : null}
+                {this.state.submitted ? <StaticP>Signed in!</StaticP> : null}
+                {this.state.location.lat ? null : <CodeInputBox type="" value={this.state.code} onChange={this.handleChange} placeholder="Enter Code..."/>}
+                {this.state.location.lat ? null : <CodeSubmitButton type="submit" value="Submit"/>}
             </EmailForm>
         )
     }
