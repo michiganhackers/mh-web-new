b79f3c7a113c1119b9f92b250735844b397117bc
KindaOK <27974340+KindaOK@users.noreply.github.com>
2020-01-23 21:09:41 -0500

    Create Login.jsx
    I think this fixes the attendance and google auth merge

---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+
+const responseGoogle = response => {
+    console.log(response);
+};
+
+class Login extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <GoogleLogin
+                    clientId="31598451640-9ch659ts8f0ik4mg5ag95213kr7ub4kr.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={"single_host_origin"}
+                />
+            </React.Fragment>
+        );
+    }
+}
+
+export default Login;
