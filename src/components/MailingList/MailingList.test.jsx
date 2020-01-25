312b3ae39f4c47e4e969266ea7b78e7c1fc47a64
Christopher Hafke <chafke@umich.edu>
2020-01-25 13:44:37 -0500

    Pointing mailjet test at new health endpoint

--- src/components/MailingList/MailingList.test.jsx
+++ src/components/MailingList/MailingList.test.jsx
@@ -2,18 +2,7 @@ import axios from "axios";
 
 // NOTE: this test does NOT consitute a thorough test of the MailingList.
 // It simply ensures that the proper environment variables are installed.
-it("can issue a non-404-ing request to the backend", () => {
+it("can verify end-to-end networking with Mailjet", () => {
     let mh_backend = process.env.REACT_APP_MH_BACKEND_URL;
-    let payload = {};
-    // An empty payload is a confirmed noop on the Mailjet side.
-    return axios({
-        method: "post",
-        headers: { "content-type": "application/json" },
-        url: mh_backend + "/v1/email/add",
-        data: JSON.stringify(payload)
-    })
-        .catch(error => {
-            expect(error.response.status).toBeTruthy();
-            expect(error.response.status).not.toEqual(404);
-        });
+    return expect(axios.get(mh_backend + "/v1/email/health")).resolves.not.toThrow();
 });
