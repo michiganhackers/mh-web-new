20900e16420362bcfffb208c9959d4a1689e1b26
Christopher Hafke <chafke@umich.edu>
2020-02-13 20:04:06 -0500

    Seeing if environment variables are working properly

--- src/components/MailingList/MailingList.test.jsx
+++ src/components/MailingList/MailingList.test.jsx
@@ -4,5 +4,6 @@ import axios from "axios";
 // It simply ensures that the proper environment variables are installed.
 it("can verify end-to-end networking with Mailjet", () => {
     let mh_backend = process.env.REACT_APP_MH_BACKEND_URL;
+    assert(mh_backend.length > 4);
     return expect(axios.get(mh_backend + "/v1/email/health")).resolves.not.toThrow();
 });
