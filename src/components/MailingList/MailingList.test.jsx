03ce07abea5ac989706d02c3e9d5540ecf0dfd10
Christopher Hafke <chafke@umich.edu>
2020-02-13 20:54:38 -0500

    Increased timeout for mailinglist test

--- src/components/MailingList/MailingList.test.jsx
+++ src/components/MailingList/MailingList.test.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// gives the backend time to spin up in case it has been sleeping.
+const MH_BACKEND_TIMEOUT_TIME = 15000;
+
 // NOTE: this test does NOT consitute a thorough test of the MailingList.
 // It simply ensures that the proper environment variables are installed.
 it("can verify end-to-end networking with Mailjet", () => {
     let mh_backend = process.env.REACT_APP_MH_BACKEND_URL;
-    expect(mh_backend.length > 4).toEqual(true);
     return expect(axios.get(mh_backend + "/v1/email/health")).resolves.not.toThrow();
-});
+}, MH_BACKEND_TIMEOUT_TIME);
