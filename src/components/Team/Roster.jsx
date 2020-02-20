671ee22e6576c6b83d064abfa44010f69ee29e96
Christopher Hafke <chafke@umich.edu>
2020-02-20 14:05:07 -0500

    Updated package.json

--- src/components/Team/Roster.jsx
+++ src/components/Team/Roster.jsx
@@ -25,39 +25,39 @@ class Roster extends Component {
         data: null
       }
   }
-  componentDidMount() {
-    this.getRoster()
-  }
-  createPerson(row){
-    return {
-      name: row.values[0].effectiveValue.stringValue,
-      team: row.values[1].effectiveValue.stringValue,
-    };
-  }
-  constructRoster(rowData){
-    let people = [];
-    for (let row of rowData){
-      if ( row.values[0].length == 0 || !row.values[0].userEnteredValue) break;
-      people.push(this.createPerson(row));
+    componentDidMount() {
+        this.getRoster();
     }
-    return people;
-  }
-  getRoster(){
-    rosterFetch()
-      .then(res => {
-        const people = this.constructRoster(res.json.sheets[0].data[0].rowData.slice(1));
-        this.setState({
-          data: people
-        });
-      })
-      .catch(res => {
-        console.log(res.error);
-      })
-  }
-  render() {
-    return ( !this.state.data ? null :
-      <React.Fragment>
-          <StaticH1>
+    createPerson(row){
+        return {
+            name: row.values[0].effectiveValue.stringValue,
+            team: row.values[1].effectiveValue.stringValue,
+        };
+    }
+    constructRoster(rowData){
+        const people = [];
+        for (const row of rowData){
+            if ( row.values[0].length === 0 || !row.values[0].userEnteredValue) break;
+            people.push(this.createPerson(row));
+        }
+        return people;
+    }
+    getRoster(){
+        rosterFetch()
+            .then(res => {
+                const people = this.constructRoster(res.json.sheets[0].data[0].rowData.slice(1));
+                this.setState({
+                    data: people
+                });
+            })
+            .catch(res => {
+                console.log(res.error);
+            });
+    }
+    render() {
+        return ( !this.state.data ? null :
+            <React.Fragment>
+                <StaticH1>
             Our Core Team
           </StaticH1>
           <TableDiv>
