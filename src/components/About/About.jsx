922039e658bb811bec63594ce377814d15a1526e
Christopher Hafke <chafke@umich.edu>
2020-09-22 20:58:16 -0400

    Revised the links/prose on the website

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -22,7 +22,7 @@ const About = () => (
                 We&apos;re a student organization at University of Michigan seeking to foster a diverse community to build and grow relevant skills in a technological and fast-paced world.
             </StaticH1>
             <StaticP>
-                Our end goal is to provide high quality resources for any and all students to become more competent members of society (technically, socially, and professionally) with a computer science twist. While connecting students to projects, skills, faculty, companies, and more, we hope to make a large university feel smaller and forge lifelong bonds in an inclusive environment while also bridging passionate people together.
+                Our end goal is to provide high quality resources for all students to become more competent technologists (technically, socially, and professionally). While connecting students to projects, skills, faculty, companies, and more, we hope to make a large university feel smaller and forge lifelong bonds in an inclusive environment.
             </StaticP>
             <MailingList />
             <StaticH1>
@@ -44,16 +44,13 @@ const About = () => (
                 Other Relevant Links
             </StaticH1>
             <StaticP>
-                Look at the following links to get a feel of our culture, the organization structure, event vibes, how the different Core Teams work, how to get involved, access to free resources, and more.
+                Look at the following links to get a feel of our culture, our organizational structure, event vibes, how our teams work, how to get involved, access to free resources, and more.
                 <EventList>
                     <li><a href="https://docs.google.com/presentation/d/122LR6C2yGpHYKe4mKHWFF98y6F8sV9Jh0dLnogiKnug/edit?usp=sharing">Our Mass Meeting Slides</a></li>
-                    <li><a href="https://docs.google.com/presentation/d/1ctHSblSXA9FiPauXlbR8ypy6dxF_zp4tH2ICU-_HKN0/edit">The Core Teams</a></li>
-                    <li><a href="https://calendar.google.com/calendar/selfsched?sstoken=UVA3dk1tZkxNOFpCfGRlZmF1bHR8ODMwMzEwNjE4YzY3Y2I0OThlZGIzY2IwMGU1MDUwMGI">Office hours signup</a></li>
+                    <li><a href="https://docs.google.com/presentation/d/1cihP20nwMc4rEwrapCwK8tumh5eUCUIdshGuB9-QoPY/edit">Our Teams</a></li>
+                    <li><a href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUZvRjlEM0RMUlZ2fGRlZmF1bHR8NjkzODc3M2I3MDJlZGM5NDQyZDU2M2IzMzc4YTM2NjY">Office hours sign-up</a></li>
                     <li><a href="https://docs.google.com/spreadsheets/d/13t39d5ou1FGmapBw2CxUsuo6OLtLENmbVQpotNGtCxM/edit#gid=0">Drop your LinkedIn</a></li>
                     <li><a href="https://docs.google.com/document/d/1wchq66lN9bDLcSSAxPEamBQYqBjUVjfJgcd9NqBc704/edit">Some CS Resources</a></li>
-                    <li><a href="http://leccap.engin.umich.edu/leccap/site/7r31hdj317ufew776zq">Recordings</a></li>
-                    <li><a href="https://drive.google.com/drive/u/0/folders/1qcobYgZCS1edagXJywzQ2ofEtGO99TOk">Photo Archive</a></li>
-                    <li><a href="https://docs.google.com/forms/d/e/1FAIpQLScQu45eXlpMcdodMqVjzdMq5xqyRBNauOqRnTfnFamhxnj6Wg/viewform?usp=sf_link">Absence Form</a></li>
                 </EventList>
             </StaticP>
             <Sponsors />
