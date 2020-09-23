7a7be8be999d13b4760ecebfdeb9b095bd7e7289
Christopher Hafke <chafke@umich.edu>
2020-09-23 12:35:34 -0400

    Fixed /u/0 in gcal links

--- src/components/Calendar/Events.jsx
+++ src/components/Calendar/Events.jsx
@@ -15,7 +15,7 @@ const Events = () => (
         <SubpageOuter header="calendar">
             <Calendar calendarStyle={CalendarStyle} />
             <a href="https://calendar.google.com/calendar?cid=OG44dTU4c3NyaWMxaG1tODRqdmt2bDlkNjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">Export/add our calendar!</a>
-            <a style={{ float: "right" }} href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUZvRjlEM0RMUlZ2fGRlZmF1bHR8NjkzODc3M2I3MDJlZGM5NDQyZDU2M2IzMzc4YTM2NjY">Office hours signup</a>
+            <a style={{ float: "right" }} href="https://calendar.google.com/calendar/selfsched?sstoken=UUZvRjlEM0RMUlZ2fGRlZmF1bHR8NjkzODc3M2I3MDJlZGM5NDQyZDU2M2IzMzc4YTM2NjY">Office hours signup</a>
         </SubpageOuter>
     </React.Fragment>
 );
