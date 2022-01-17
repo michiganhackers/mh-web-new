6c7ab4bab19de1af1548ed7b64769fb22772150f
KindaOK <27974340+KindaOK@users.noreply.github.com>
2022-01-16 21:42:36 -0500

    Update About.jsx
    Fixed a dom error with block-level elements in inline elements.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,6 +12,8 @@ const EventList = styled.ul`
   list-style-type: none;
   margin: auto;
   padding: 0;
+  font-size: 1.2em;
+  text-align: center;
 `;
 
 const About = () => (
@@ -30,29 +32,29 @@ const About = () => (
             </StaticH1>
             <StaticP>
                 Some events we&apos;ve had in the past or are planning for the future include:
-                <EventList>
-                    <li>Weekly Hack Nights</li>
-                    <li>Company Events</li>
-                    <li>Organizing groups to attend hackathons</li>
-                    <li>Lightning Talks</li>
-                    <li>Cybersecurity Capture The Flag competitions</li>
-                    <li>Resume Critiques</li>
-                    <li>Interview Prep</li>
-                </EventList>
             </StaticP>
+            <EventList>
+                <li>Weekly Hack Nights</li>
+                <li>Company Events</li>
+                <li>Organizing groups to attend hackathons</li>
+                <li>Lightning Talks</li>
+                <li>Cybersecurity Capture The Flag competitions</li>
+                <li>Resume Critiques</li>
+                <li>Interview Prep</li>
+            </EventList>
             <StaticH1>
                 Other Relevant Links
             </StaticH1>
             <StaticP>
                 Look at the following links to get a feel of our culture, our organizational structure, event vibes, how our teams work, how to get involved, access to free resources, and more.
-                <EventList>
-                    <li><a href="https://docs.google.com/presentation/d/11QyTngMyUgGYdRbpwM3XfDPIQ9NIT4QieeWnN1O0HJY/edit?usp=sharing">Our Mass Meeting Slides</a></li>
-                    {/*<li><a href="https://docs.google.com/presentation/d/1cihP20nwMc4rEwrapCwK8tumh5eUCUIdshGuB9-QoPY/edit">Our Teams</a></li>*/}
-                    <li><a href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUh5amplSG5xVkVMfGRlZmF1bHR8MmFhMDQ2ZTllMzlmMmZmZDU4ZjAwNDZkNGNiOTZiMDU">Office hours sign-up</a></li>
-                    {/*<li><a href="https://docs.google.com/spreadsheets/d/13t39d5ou1FGmapBw2CxUsuo6OLtLENmbVQpotNGtCxM/edit#gid=0">Drop your LinkedIn</a></li>*/}
-                    <li><a href="https://docs.google.com/document/d/1wchq66lN9bDLcSSAxPEamBQYqBjUVjfJgcd9NqBc704/edit">Some CS Resources</a></li>
-                </EventList>
             </StaticP>
+            <EventList>
+                <li><a href="https://docs.google.com/presentation/d/11QyTngMyUgGYdRbpwM3XfDPIQ9NIT4QieeWnN1O0HJY/edit?usp=sharing">Our Mass Meeting Slides</a></li>
+                {/*<li><a href="https://docs.google.com/presentation/d/1cihP20nwMc4rEwrapCwK8tumh5eUCUIdshGuB9-QoPY/edit">Our Teams</a></li>*/}
+                <li><a href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUh5amplSG5xVkVMfGRlZmF1bHR8MmFhMDQ2ZTllMzlmMmZmZDU4ZjAwNDZkNGNiOTZiMDU">Office hours sign-up</a></li>
+                {/*<li><a href="https://docs.google.com/spreadsheets/d/13t39d5ou1FGmapBw2CxUsuo6OLtLENmbVQpotNGtCxM/edit#gid=0">Drop your LinkedIn</a></li>*/}
+                <li><a href="https://docs.google.com/document/d/1wchq66lN9bDLcSSAxPEamBQYqBjUVjfJgcd9NqBc704/edit">Some CS Resources</a></li>
+            </EventList>
             <Sponsors />
         </SubpageOuter>
     </React.Fragment>
