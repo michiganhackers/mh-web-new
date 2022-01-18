0da42ccbcf51bbec98b9dfa0c7eb0e4d27ce9e69
KindaOK <27974340+KindaOK@users.noreply.github.com>
2022-01-18 13:50:47 -0500

    Update ScrollToTop.jsx
    Added jsdoc

--- src/utility/ScrollToTop.jsx
+++ src/utility/ScrollToTop.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+/**
+ * Include in page to automatically scroll to the top of the page on navigation
+ * https://v5.reactrouter.com/web/guides/scroll-restoration
+ * @constructor
+ */
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
