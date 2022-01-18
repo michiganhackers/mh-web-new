fcd20917b849ffa4ff42834f493a65618bff9dd4
KindaOK <27974340+KindaOK@users.noreply.github.com>
2022-01-17 23:49:09 -0500

    Updated/removed dependencies
    Updated react to 17. Removed several unused dependencies. Removed some ESLint rules that conflict with prettier. Also moved some Create React App dependencies up to package.json as per https://github.com/facebook/create-react-app/issues/11906 and https://github.com/facebook/create-react-app/pull/11474. Updated the .eslintrc.json file to reflect updates to ESLint and babel. Also removed package-lock.json to prevent confusion. Pain.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 // This gets called before any tests run. Put any such setup tasks here.
 configure({ adapter: new Adapter() });
