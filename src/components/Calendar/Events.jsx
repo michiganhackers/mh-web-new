95e6e26ba292c5efeb92d81fa38c44e1964557d6
KindaOK <27974340+KindaOK@users.noreply.github.com>
2022-08-30 01:01:32 -0400

    Remove Office hours signup from calendar

--- src/components/Calendar/Events.jsx
+++ src/components/Calendar/Events.jsx
@@ -17,12 +17,6 @@ const Events = () => (
             <a href="https://calendar.google.com/calendar?cid=OG44dTU4c3NyaWMxaG1tODRqdmt2bDlkNjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">
                 Export/add our calendar!
             </a>
-            <a
-                style={{ float: "right" }}
-                href="https://calendar.google.com/calendar/selfsched?sstoken=UUZvRjlEM0RMUlZ2fGRlZmF1bHR8NjkzODc3M2I3MDJlZGM5NDQyZDU2M2IzMzc4YTM2NjY"
-            >
-                Office hours signup
-            </a>
         </SubpageOuter>
     </React.Fragment>
 );
