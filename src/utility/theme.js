a9135ff3f863b74855ca2014a5ed2a8f34129eee
KindaOK <27974340+KindaOK@users.noreply.github.com>
2023-01-01 23:00:56 -0500

    Create theme file and add colors
    Color names need something better so it's easier to integrate them.

---
+++ src/utility/theme.js
@@ -0,0 +1,25 @@
+const hackerOrange = "#f15d24";
+
+export const lightTheme = {
+  hackerOrange,
+  primary: "#ff774a",
+
+  primaryDark: "#ee5f30",
+  primaryDarker: "#c74421",
+
+  primaryLight: "#ed8246",
+  primaryLighter: "#f18048",
+  primaryLightest: "#f38a63",
+  primaryPale: "#ffaf92",
+
+  secondary: "#8dcadf",
+  secondaryBright: "#59caf1",
+  secondaryLight: "#a9d2de",
+  secondaryLighter: "#c8e4dd",
+
+  gray: "#d9d9d9",
+  white: "#fff",
+  black: "#000",
+
+};
+
