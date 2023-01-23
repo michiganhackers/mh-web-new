4d03ea261ffefaa260e1d519004a07924d734ac0
Adam Gunn <50597057+adamgunn@users.noreply.github.com>
2023-01-23 04:11:32 -0500

    begun reworking design for sidebar instead of top buttons

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -1,164 +1,66 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import "utility/fonts.css";
 import Navbar from "components/Navbar.jsx";
 import styled from "styled-components";
 import teams from "teams.json";
 import SubteamCard from "components/Teams/SubteamCard.jsx";
-import devices from "utility/MediaQueries.js";
+// import devices from "utility/MediaQueries.js";
 
-function useWindowSize() {
-    const [windowSize, setWindowSize] = useState({
-      width: undefined,
-      height: undefined,
-    });
-    useEffect(() => {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-      window.addEventListener("resize", handleResize);
-      handleResize();
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
-    return windowSize;
-  }
+// function useWindowSize() {
+//     const [windowSize, setWindowSize] = useState({
+//       width: undefined,
+//       height: undefined,
+//     });
+//     useEffect(() => {
+//       function handleResize() {
+//         setWindowSize({
+//           width: window.innerWidth,
+//           height: window.innerHeight,
+//         });
+//       }
+//       window.addEventListener("resize", handleResize);
+//       handleResize();
+//       return () => window.removeEventListener("resize", handleResize);
+//     }, []);
+//     return windowSize;
+//   }
 
-const OurSubteamsWrapper = styled.nav`
-    display: flex;
-    justify-content: center;
-    margin: 0;
-    padding: 0;
-    height: min-content;
-`;
-
-const OurSubsteamsDiv = styled.div`
-    margin: 1rem 1rem 0;
-    border-radius: 10px;
-    width: 100%;
-    max-width: 1200px;
-    background-color: #ED8246;
-    padding: 1rem;
-
-    ${devices.tablet`
-        max-width: 500px;
-    `}
-`;
-
-const OurSubteamsTitle = styled.h1`
-    color: white;
-    text-align: center;
-    font-weight: bold;
-`;
-
-const SubteamButtonsGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 220px);
-    ${devices.tablet`
-        grid-template-columns: repeat(2, 200px);
-    `}
-    ${devices.small`
-        grid-template-columns: repeat(2, 150px);
-    `}
-    ${devices.tiny`
-        grid-template-columns: repeat(2, 120px);
-    `}
-    gap: 1rem;
-`;
-
-const SubteamButtonsGridWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-`;
-
-const SubteamButtonWrapper = styled.div`
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 220px;
-    ${devices.tablet`
-        width: 200px;
-    `}
-    ${devices.small`
-        width: 150px;
-    `}
-    ${devices.tiny`
-        width: 120px;
-    `}
-    min-height: 40px;
-    border: 2px solid white;
-    background-color: white;
-    border-radius: 8px;
-
-    &:hover {
-        border: 2px solid white;
-        background-color: #F18048;
-        cursor: pointer;
-    }
-
-    &:hover > p {
-        color: white;
-    }
-    transition: all 0.2s;
-`;
-
-const SubteamButtonText = styled.p`
-    color: #F18048;
-    outline: none;
-    font-size: 1rem;
-    display: block;
-    text-decoration: none;
-    text-align: center;
-    margin: 0;
-
-    &:hover {
-        text-decoration: none;
-    }
-    &:focus {
-        outline: none;
-    }
-`;
-
-const SubTeamButtonLink = styled.a`
-    &:hover {
-        text-decoration: none;
-    }
-`;
-
-const SubteamButtonCenterer = styled.div`
-    grid-column: 1 / span 3;
-    a:last-child {
-        margin-left: 1rem;
-    }
-    ${devices.tablet`
-        grid-column: 1 / span 2;
-        a:last-child {
-            margin-left: 0;
-        }
-    `}
-    ${devices.tiny`
-        grid-column: 1 / span 2;
-        a:last-child {
-            margin-left: 0;
-        }
-    `}
-    display: flex;
-    justify-content: center;
-
-`;
 
 const SubteamCardsDiv = styled.div`
     padding: 0 1rem;
+`;
+
+const SidebarWrapper = styled.div`
+    background-color: #eee;
+`;
+
+const Sidebar = styled.div`
+    padding: 2rem;
+    width: inherit;
+    position: fixed;
+    width: 400px;
+`;
+const PageLayout = styled.div`
+    display: grid;
+    grid-template-columns: 400px 1fr;
+`;
+
+const SidebarLink = styled.a`
+    display: block;
+    padding: 1rem;
+    width: 100%;
+
+    &:hover {
+        background-color: #ddd;
+        cursor: pointer;
+    }
 `;
 
 const Teams = () => {
     const teamNames = teams.map(team => team.name);
     const teamIds = teamNames.map(name => name.replaceAll(" ", "_").toLowerCase());
     const cardsRef = useRef([]);
-    const windowSize = useWindowSize();
+    // const windowSize = useWindowSize();
 
     useEffect(() => {
         cardsRef.current = cardsRef.current.slice(0, teams.length);
@@ -193,51 +95,16 @@ const Teams = () => {
     return (
         <>
             <Navbar />
-            <OurSubteamsWrapper>
-                <OurSubsteamsDiv>
-                    <OurSubteamsTitle>Our Subteams</OurSubteamsTitle>
-                    <SubteamButtonsGridWrapper>
-                        <SubteamButtonsGrid>
-                        {windowSize.width < 768 ? 
-                            teamNames.map((teamName, i) => 
-                                i === teams.length - 1 ? 
-                                <SubteamButtonCenterer>
-                                    <SubTeamButtonLink key={i} href={`#${teamIds[i]}`} onClick={(e) => handleClick(e, i)}>
-                                        <SubteamButtonWrapper>
-                                            <SubteamButtonText>{teamName}</SubteamButtonText>
-                                        </SubteamButtonWrapper>
-                                    </SubTeamButtonLink>
-                                </SubteamButtonCenterer>
-                                :
-                                <SubTeamButtonLink key={i} href={`#${teamIds[i]}`} onClick={(e) => handleClick(e, i)}>
-                                    <SubteamButtonWrapper>
-                                        <SubteamButtonText>{teamName}</SubteamButtonText>
-                                    </SubteamButtonWrapper>
-                                </SubTeamButtonLink>
-                            )
-                        :
-                            teamNames.slice(0, -2).map((teamName, i) => 
-                                <SubTeamButtonLink key={i} href={`#${teamIds[i]}`} onClick={(e) => handleClick(e, i)}>
-                                    <SubteamButtonWrapper>
-                                        <SubteamButtonText>{teamName}</SubteamButtonText>
-                                    </SubteamButtonWrapper>
-                                </SubTeamButtonLink>
-                            ).concat(
-                                <SubteamButtonCenterer>
-                                    {teamNames.slice(-2).map((teamName, i) => 
-                                        <SubTeamButtonLink key={i} href={`#${teamIds[i]}`} onClick={(e) => handleClick(e, i + teamNames.length - 2)}>
-                                            <SubteamButtonWrapper>
-                                                <SubteamButtonText>{teamName}</SubteamButtonText>
-                                            </SubteamButtonWrapper>
-                                        </SubTeamButtonLink>
-                                    )}
-                                </SubteamButtonCenterer>
-                            )
-                        }
-                        </SubteamButtonsGrid>
-                    </SubteamButtonsGridWrapper>
-                </OurSubsteamsDiv>
-            </OurSubteamsWrapper>
+            <PageLayout>
+            <SidebarWrapper>
+                <Sidebar>
+                    {teamNames.map((team, i) => 
+                        <SidebarLink key={i} onClick={e => handleClick(e, i)}>
+                            {team}
+                        </SidebarLink>
+                    )}
+                </Sidebar>
+            </SidebarWrapper>
             <SubteamCardsDiv>
                 {teams.map((team, i) => 
                     <SubteamCard
@@ -250,6 +117,7 @@ const Teams = () => {
                     />
                 )}
             </SubteamCardsDiv>
+            </PageLayout>
         </>
     );
 };
