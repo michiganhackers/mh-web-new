f7497933202172c08691d71706d070b91be240c2
KindaOK <27974340+KindaOK@users.noreply.github.com>
2023-03-22 20:09:06 -0400

    Add padding between footer and calendar export link
    Elements were previously too close to each other.

--- src/components/Calendar/Events.jsx
+++ src/components/Calendar/Events.jsx
@@ -4,20 +4,26 @@ import SubpageOuter from "../SubpageOuter/SubpageOuter.jsx";
 import Calendar from "./Calendar.jsx";
 import Navbar from "../Navbar.jsx";
 import Footer from "components/Footer.jsx";
+import styled from "styled-components";
 
 const CalendarStyle = {
     color: "#F15D24",
     textColor: "white",
 };
 
+const ExportLink = styled.a`
+    padding-bottom: 24px;
+    display: inline-block;
+`;
+
 const Events = () => (
     <React.Fragment>
         <Navbar />
         <SubpageOuter header="calendar">
             <Calendar calendarStyle={CalendarStyle} />
-            <a href="https://calendar.google.com/calendar?cid=OG44dTU4c3NyaWMxaG1tODRqdmt2bDlkNjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">
+            <ExportLink href="https://calendar.google.com/calendar?cid=OG44dTU4c3NyaWMxaG1tODRqdmt2bDlkNjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">
                 Export/add our calendar!
-            </a>
+            </ExportLink>
         </SubpageOuter>
         <Footer />
     </React.Fragment>
