98fb746ef36dd2d0120187b01e75a4b41ff1d46b
Cooper Saye <coopersaye@gmail.com>
2023-08-20 21:25:35 -0700

    Update calendar

--- src/components/Calendar/Events.jsx
+++ src/components/Calendar/Events.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 import "utility/fonts.css";
-import SubpageOuter from "components/SubpageOuter/SubpageOuter.jsx";
+
 import Calendar from "./Calendar.jsx";
 import Navbar from "components/Navbar.jsx";
 import { SubTheme } from "components/ThemeComponents.jsx";
 import Footer from "components/Footer.jsx";
 import styled from "styled-components";
 
-const CalendarStyle = {
-    color: "#F15D24",
-    textColor: "white",
-};
+export default function Events() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <SubTheme name="events">
+                <Content>
+                    <h1>Calendar</h1>
+                    <Calendar />
+                </Content>
+            </SubTheme>
+            <Footer />
+        </React.Fragment>
+    );
+}
 
-const ExportLink = styled.a`
-    padding-bottom: 24px;
-    display: inline-block;
+const Content = styled.div`
+    color: #fff;
+    text-align: center;
 `;
-
-const Events = () => (
-    <React.Fragment>
-        <Navbar />
-        <SubTheme name="events">
-            <SubpageOuter header="calendar">
-                <SubTheme name="calendar">
-                    <Calendar calendarStyle={CalendarStyle} />
-                </SubTheme>
-                <ExportLink href="https://calendar.google.com/calendar?cid=OG44dTU4c3NyaWMxaG1tODRqdmt2bDlkNjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">
-                    Export/add our calendar!
-                </ExportLink>
-            </SubpageOuter>
-        </SubTheme>
-        <Footer />
-    </React.Fragment>
-);
-
-export default Events;
