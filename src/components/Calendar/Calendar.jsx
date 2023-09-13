3f48ab859bb6ad4e90d00648d431805ffe32a759
Cooper Saye <coopersaye@gmail.com>
2023-09-12 21:57:03 -0400

    Update calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -5,7 +5,7 @@ export default function Calendar() {
   return (
     <CalendarRoot>
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=michiganhackers%40gmail.com&ctz=America%2FNew_York"
+        src="https://calendar.google.com/calendar/embed?src=909ff6cc12130251f59ee890ee7ef3aedef47d3167a8df530fea96199c7db2d7%40group.calendar.google.com&ctz=America%2FNew_York"
         frameBorder="0"
         scrolling="no"
       />
